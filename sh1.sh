0ca0e2d6113b74fa93bfdbd44ac2e72f6cb25dd2
liuran001 <32791471+liuran001@users.noreply.github.com>
2020-08-30 14:00:42 +0800

    Update sh1.sh

--- sh1.sh
+++ sh1.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-rename README.md README1.md
 svn co https://github.com/kenzok8/openwrt-packages/trunk ./
 rm -rf .svn
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw ./
@@ -7,5 +6,4 @@ rm -rf .svn
 svn co https://github.com/cnzd/luci-app-koolproxyR/trunk ./luci-app-koolproxyR
 rm -rf .svn
 rm README.md
-rename README1.md README.md
 exit 0
