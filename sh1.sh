70f5ab0de955570f8aa8b3a055e50ec0b239a887
liuran001 <32791471+liuran001@users.noreply.github.com>
2020-08-30 14:03:41 +0800

    Update sh1.sh

--- sh1.sh
+++ sh1.sh
@@ -5,5 +5,6 @@ svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw ./
 rm -rf .svn
 svn co https://github.com/cnzd/luci-app-koolproxyR/trunk ./luci-app-koolproxyR
 rm -rf .svn
-rm README.md
+svn co https://github.com/xiaorouji/openwrt-package/trunk ./
+rm README.md .gitattributes .gitignore
 exit 0
