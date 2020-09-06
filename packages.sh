37b67ed5a24eb6b26b25d6341614b896708b3e87
liuran001 <32791471+liuran001@users.noreply.github.com>
2020-09-06 12:45:24 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -5,7 +5,9 @@ svn co https://github.com/rufengsuixing/luci-app-adguardhome/trunk ./luci-app-ad
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-advancedsetting
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-aliddns
 svn co https://github.com/frainzy1477/luci-app-clash/trunk ./luci-app-clash
-svn co https://github.com/vernesong/OpenClash/branches/master/luci-app-openclash
+git clone https://github.com/vernesong/OpenClash
+mv -rf OpenClash/luci-app-openclash .
+rm -rf OpenClash
 svn co https://github.com/garypang13/luci-app-eqos/trunk ./luci-app-eqos
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-app-gost
 svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk ./luci-app-jd-dailybonus
